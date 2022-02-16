af72c2100f88f94d23227e010791b36842d1d65d
HannoVB1 <hannovb@hotmail.be>
2022-02-16 20:35:29 +0100

    oef 2 en 3

--- oefening2/scripts/driversLicense.js
+++ oefening2/scripts/driversLicense.js
@@ -4,3 +4,13 @@ const speedLimit = 70;
 const kmPerPoint = 5;
 
 let snelheid = prompt("hoe snel gaat u?");
+
+if (snelheid <= speedLimit) {
+  console.log("OK");
+}
+else if (snelheid > speedLimit) {
+  var teSnel = snelheid - speedLimit;
+  var punten = Math.round((teSnel - (teSnel % kmPerPoint)) / kmPerPoint);
+
+  console.log("Strafpunten: " + punten);
+}
--- oefening3/scripts/sumOfMultiples.js
+++ oefening3/scripts/sumOfMultiples.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const getal = prompt(     "Geef een getal");
+var totaleSom3 = 0;
+var totaleSom5 = 0;
+
+if (getal >= 3) {
+  for (var i = 3; i <= getal; i+=3) {
+    totaleSom3 += i;
+  }
+}
+
+console.log(totaleSom3);
+if (getal >= 5) {
+  for (var i = 5; i <= getal; i+=5) {
+    totaleSom5 += i;
+  }
+}
+console.log(totaleSom5);
+
+var totaal = totaleSom3 + totaleSom5;
+console.log(totaal);
